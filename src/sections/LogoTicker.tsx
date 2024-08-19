110b05b2c1879dcf664ddf90ff86a4f487ce6ee2
0xtomotech <0xtomotech@gmail.com>
2024-08-19 15:41:27 +0200

    added hero and logo ticker animations

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import acmeLogo from "../assets/logo-acme.png";
 import apexLogo from "../assets/logo-apex.png";
 import celestialLogo from "../assets/logo-celestial.png";
 import quantumLogo from "../assets/logo-quantum.png";
 import pulseLogo from "../assets/logo-pulse.png";
 import echoLogo from "../assets/logo-echo.png";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
+  const logos = [
+    acmeLogo,
+    apexLogo,
+    celestialLogo,
+    quantumLogo,
+    pulseLogo,
+    echoLogo,
+  ];
+
   return (
     <section className="py-20 md:py-24">
       <div className="container">
@@ -13,19 +25,30 @@ export const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovative teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
-              {[
-                acmeLogo,
-                apexLogo,
-                celestialLogo,
-                quantumLogo,
-                pulseLogo,
-                echoLogo,
-              ].map((logo, index) => (
-                <img src={logo.src} key={logo.src} className="h-6 w-auto" />
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{
+                translateX: "-50%",
+              }}
+              animate={{
+                translateX: "0",
+              }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: "linear",
+              }}
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+            >
+              {logos.concat(logos).map((logo, index) => (
+                <img
+                  src={logo.src}
+                  key={`${logo.src}-${index}`}
+                  className="h-6 w-auto"
+                  alt={`Logo ${index + 1}`}
+                />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
